3fcf952e98843c6d552298d936cf50d3d9c91164
Zachary Murphy <murphyz@my.wccc.edu>
2016-03-21 20:56:50 -0400

    touch up

--- Murphy_Zachary_HandsOn07/HandsOnProject7-4/script.js
+++ Murphy_Zachary_HandsOn07/HandsOnProject7-4/script.js
@@ -11,44 +11,33 @@
 
 "use strict";
  
- //global variabe, and empty object
  var delivInfo = {};
  
- //global variable, points to the element with the id value
  
  var delivSummary = document.getElementById("deliverTo");
  
- // this function will add the value of each input field to the delivInfo object as a property
  function processDeliveryInfo()
  {
 	 var prop;
 	 
-	 //add statements that store values into the delivInfo object and various properties
 	 delivInfo.name = document.getElementById("nameinput").value;
 	 delivInfo.addr = document.getElementById("addrinput").value;
 	 delivInfo.city = document.getElementById("cityinput").value;
 	 delivInfo.email = document.getElementById("emailinput").value;
 	 delivInfo.phone = document.getElementById("phoneinput").value;
 	 
-	 //add a for/in Loop with a subsequent statements
 	 for (prop in delivInfo)
 	 {
-		 //This statement adds the value of the current property to the content of the deliverTo element
 		 delivSummary.innerHTML += "<p>" + delivInfo[prop] + "</p>";
 	 }
-		//change the display style of this section to block
 		document.getElementById("deliverTo").style.display = "block";
  }
- //function
  function previewOrder()
  {
-	 //a function call
 	 processDeliveryInfo();
-	 //change the display style of this section to block
 	 document.getElementsByTagName("section")[0].style.display = "block";
  }
  
- //create event listener that calls the prevewOrder function
  function createEventListener()
  {
 	 var previewBtn = document.getElementById("previewBtn");
@@ -62,7 +51,6 @@
 	 }
  }
  
- //create event listener when page finishes loading
  if (window.addEventListener)
  {
 	 window.addEventListener("load", createEventListener, false);
